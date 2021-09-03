e494e3272a56e48f5cdf04c8a11f53802314dc41
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-09-03 11:28:21 +0200

    Sample: Add sample extension project

---
+++ Celones.Windows.FileManager.SampleExtension/SampleExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Celones.Windows.FileManager.SampleExtension
+{
+    public class SampleExtension : FileManagerExtension
+    {
+        private static SampleExtension _extension = new();
+
+        [UnmanagedCallersOnly(EntryPoint = "FMExtensionProcW")]
+        public static int EntryPoint(IntPtr hWnd, IntPtr wEvent, IntPtr lParam)
+            => _extension.ExtensionProc(hWnd, wEvent, lParam);
+
+        protected override bool OnLoad(LoadEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show(
+                text: "Hello World!",
+                caption: "Windows File Manager managed extension",
+                System.Windows.Forms.MessageBoxButtons.OK,
+                System.Windows.Forms.MessageBoxIcon.Exclamation);
+            return true;
+        }
+    }
+}
