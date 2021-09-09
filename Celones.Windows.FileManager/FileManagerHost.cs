48800d02668d35dab98112b32f5f68f7b630c630
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-09-10 00:16:15 +0200

    API: Properly marshal File Manager calls

--- Celones.Windows.FileManager/FileManagerHost.cs
+++ Celones.Windows.FileManager/FileManagerHost.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Vanara.PInvoke;
 using static Celones.Windows.FileManager.Interop;
 
@@ -17,26 +19,48 @@ namespace Celones.Windows.FileManager
         public int SelectionCountShortNames => (int)User32.SendMessage(_hWnd, FM_GETSELCOUNT);
         public int SelectionCount => (int)User32.SendMessage(_hWnd, FM_GETSELCOUNTLFN);
 
-        public DriveInfo SelectedDrive {
+        public DriveInfo SelectedDrive
+        {
             get
             {
-                var info = new FMS_GETDRIVEINFOW();
-                User32.SendMessage(_hWnd, FM_GETDRIVEINFO, 0, ref info);
+                var buffer = Marshal.AllocHGlobal(Marshal.SizeOf<FMS_GETDRIVEINFOW>());
+                User32.SendMessage(_hWnd, FM_GETDRIVEINFO, IntPtr.Zero, (long)buffer);
+
+                var info = Marshal.PtrToStructure<FMS_GETDRIVEINFOW>(buffer);
+                Marshal.FreeHGlobal(buffer);
                 return new DriveInfo(info);
+            }
+        }
+
+        public IEnumerable<FileInfo> SelectedFiles
+        {
+            get
+            {
+                var count = SelectionCount;
+                for (var index = 0; index < count; index++)
+                {
+                    yield return GetSelectedFile(index);
+                }
             }
         }
 
         public FileInfo GetSelectedFileShortNames(int index)
         {
-            var info = new FMS_GETFILESELW();
-            User32.SendMessage(_hWnd, FM_GETFILESEL, index, ref info);
+            var buffer = Marshal.AllocHGlobal(Marshal.SizeOf<FMS_GETFILESELW>());
+            User32.SendMessage(_hWnd, FM_GETFILESELLFN, (IntPtr)index, (long)buffer);
+
+            var info = Marshal.PtrToStructure<FMS_GETFILESELW>(buffer);
+            Marshal.FreeHGlobal(buffer);
             return new FileInfo(info);
         }
 
         public FileInfo GetSelectedFile(int index)
         {
-            var info = new FMS_GETFILESELW();
-            User32.SendMessage(_hWnd, FM_GETFILESELLFN, index, ref info);
+            var buffer = Marshal.AllocHGlobal(Marshal.SizeOf<FMS_GETFILESELW>());
+            User32.SendMessage(_hWnd, FM_GETFILESELLFN, (IntPtr)index, (long)buffer);
+
+            var info = Marshal.PtrToStructure<FMS_GETFILESELW>(buffer);
+            Marshal.FreeHGlobal(buffer);
             return new FileInfo(info);
         }
 
