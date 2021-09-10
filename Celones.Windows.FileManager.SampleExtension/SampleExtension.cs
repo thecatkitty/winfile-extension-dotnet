7ce1590533316b4f70e3b4632750534db3b088bb
Mateusz Karcz <mateusz.karcz@interia.eu>
2021-09-10 10:09:21 +0200

    Sample: Composition over inheritance

--- Celones.Windows.FileManager.SampleExtension/SampleExtension.cs
+++ Celones.Windows.FileManager.SampleExtension/SampleExtension.cs
@@ -6,76 +6,76 @@ using System.Linq;
 
 namespace Celones.Windows.FileManager.SampleExtension
 {
-    public class SampleExtension : FileManagerExtension
+    public static class SampleExtension
     {
         private const int IDM_FIRSTBUTTON = 1;
         private const int IDM_TESTMENU = 2;
         private const int IDM_TOGGLE = 8;
 
-        private static readonly SampleExtension s_extension = new();
+        private static readonly FileManagerExtension s_extension;
+        private static readonly HINSTANCE s_instance;
+        private static uint s_menuDelta;
+        private static bool s_toggled;
 
         [UnmanagedCallersOnly(EntryPoint = "FMExtensionProcW")]
         public static int EntryPoint(IntPtr hWnd, IntPtr wEvent, IntPtr lParam)
             => s_extension.ExtensionProc(hWnd, wEvent, lParam);
 
-        private readonly HINSTANCE _instance;
-        private uint _menuDelta;
-        private bool _toggled;
-
-        public SampleExtension()
+        static SampleExtension()
         {
             var name = typeof(SampleExtension).Namespace + ".dll";
-            _instance = Kernel32.EnumProcessModules(Kernel32.GetCurrentProcess())
+            s_instance = Kernel32.EnumProcessModules(Kernel32.GetCurrentProcess())
                 .First(module => Kernel32.GetModuleFileName(module).EndsWith(name));
+
+            s_extension = new();
+            s_extension.Load += LoadEventHandler;
+            s_extension.MenuInitialize += MenuInitializeEventHandler;
+            s_extension.ToolbarLoad += ToolbarLoadEventHandler;
+            s_extension.HelpString += HelpStringEventHandler;
+            s_extension.ContextHelp += ContextHelpEventHandler;
+            s_extension.Command += CommandEventHandler;
         }
 
-        protected override bool OnLoad(LoadEventArgs e)
+        private static void LoadEventHandler(object sender, LoadEventArgs e)
         {
             e.MenuName = "AddonSampleMenu";
-            e.MenuHandle = User32.GetSubMenu(User32.LoadMenu(_instance, 101), 0);
-            _menuDelta = e.MenuDelta;
-
-            return base.OnLoad(e);
+            e.MenuHandle = User32.GetSubMenu(User32.LoadMenu(s_instance, 101), 0);
+            s_menuDelta = e.MenuDelta;
         }
 
-        protected override bool OnMenuInitialize(MenuInitializeEventArgs e)
+        private static void MenuInitializeEventHandler(object sender, MenuInitializeEventArgs e)
         {
             User32.CheckMenuItem(
                 hMenu: e.Menu,
-                uIDCheckItem: _menuDelta + 8,
-                uCheck: _toggled
+                uIDCheckItem: s_menuDelta + IDM_TOGGLE,
+                uCheck: s_toggled
                 ? User32.MenuFlags.MF_BYCOMMAND | User32.MenuFlags.MF_CHECKED
                 : User32.MenuFlags.MF_BYCOMMAND | User32.MenuFlags.MF_UNCHECKED);
-
-            return base.OnMenuInitialize(e);
         }
 
-        protected override bool OnToolbarLoad(ToolbarLoadEventArgs e)
+        private static void ToolbarLoadEventHandler(object sender, ToolbarLoadEventArgs e)
         {
-            e.Buttons.Add(new ToolbarButton { CommandId = 1 });
+            e.Buttons.Add(new ToolbarButton { CommandId = IDM_FIRSTBUTTON });
             e.BitmappedCount = 1;
             e.BitmapId = 102;
-            return base.OnToolbarLoad(e);
         }
 
-        protected override bool OnHelpString(HelpStringEventArgs e)
+        private static void HelpStringEventHandler(object sender, HelpStringEventArgs e)
         {
             e.Help = e.CommandId switch
             {
                 -1 => "The mouse is over the menu item (Extension).",
                 _ => $"Tooltip for item {e.CommandId}"
             };
-            return base.OnHelpString(e);
         }
 
-        protected override bool OnContextHelp(ContextHelpEventArgs e)
+        private static void ContextHelpEventHandler(object sender, ContextHelpEventArgs e)
         {
             User32.MessageBox(e.Host.Window, $"Help for {e.CommandId}", "WinHelp call", User32.MB_FLAGS.MB_OK);
             // User32.WinHelp(e.Window, "ExtHelp.hlp", User32.HelpCmd.HELP_CONTEXT, (IntPtr)e.CommandId);
-            return base.OnContextHelp(e);
         }
 
-        protected override int OnCommand(CommandEventArgs e)
+        private static void CommandEventHandler(object sender, CommandEventArgs e)
         {
             switch (e.CommandId)
             {
@@ -106,20 +106,18 @@ namespace Celones.Windows.FileManager.SampleExtension
 
                 case IDM_TOGGLE:
                 {
-                    User32.MessageBox(e.Host.Window, _toggled ? "Hi On" : "Hi Off", "IDM_TOGGLE", User32.MB_FLAGS.MB_OK);
-                    _toggled = !_toggled;
+                    User32.MessageBox(e.Host.Window, s_toggled ? "Hi On" : "Hi Off", "IDM_TOGGLE", User32.MB_FLAGS.MB_OK);
+                    s_toggled = !s_toggled;
                     break;
                 }
 
                 default:
                 {
                     User32.MessageBox(e.Host.Window, $"Unrecognized idm: {e.CommandId}", "Error", User32.MB_FLAGS.MB_OK);
-                    _toggled = !_toggled;
-                    return base.OnCommand(e);
+                    e.ReturnValue = -1;
+                    break;
                 }
             }
-
-            return 0;
         }
     }
 }
